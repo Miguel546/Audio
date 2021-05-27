76e3cf64b58ff8d7e03c332deec6805bf78be4d4
Luis Miguel Casas <mcasasq@gmail.com>
2021-05-26 19:46:49 -0500

    boton grabar no funciona

--- app/src/main/java/com/luismiguel/audio/MainActivity.kt
+++ app/src/main/java/com/luismiguel/audio/MainActivity.kt
@@ -8,40 +8,55 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
+import android.widget.Toast
 import java.io.IOException
 
 class MainActivity : AppCompatActivity(), MediaPlayer.OnPreparedListener {
     var buttonGrabar: Button ? = null
     var buttonReproducir: Button ? = null
+    var buttonReproducirSonido: Button ? = null
     var mp : MediaPlayer ? = null
-    val CODIGO_GRABAR = 50
+    val CODIGO_GRABAR = 1
     var uri : Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonGrabar = findViewById(R.id.btnGrabar)
         buttonReproducir = findViewById(R.id.btnReproducir)
+        buttonReproducirSonido = findViewById(R.id.btnReproducirSonido)
         buttonGrabar?.setOnClickListener{
             grabar()
         }
         buttonReproducir?.setOnClickListener{
             reproducirGrabacion()
         }
+
+        buttonReproducirSonido?.setOnClickListener{
+            sonido()
+        }
+    }
+
+    fun sonido(){
+        val mediaPlayer = MediaPlayer.create(this, R.raw.happy).start()
     }
 
     override fun onPrepared(mp: MediaPlayer?) {
-        mp!!.start()
+        mp?.start()
     }
 
     fun grabar(){
         val intent = Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION)
-        startActivityForResult(intent, CODIGO_GRABAR)
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+        } else {
+            Toast.makeText(this, "No sound record application found", Toast.LENGTH_SHORT).show();
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == CODIGO_GRABAR && resultCode == Activity.RESULT_OK){
-            uri = data!!.data
+            uri = data?.data
         }
     }
 
